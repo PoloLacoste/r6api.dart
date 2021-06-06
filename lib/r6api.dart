eb81872669596e760bc81a4bbdf9d8fabb3ff9f1
JM REMEUR <jm@remeur.fr>
2021-06-06 19:30:34 +0200

    init package

---
+++ lib/r6api.dart
@@ -0,0 +1,7 @@
+library r6api;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/r6api_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:r6api/r6api.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
